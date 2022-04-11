8de96766c41721da3c9f0a0382f7cb29dae9f039
Abhijeet Salve <101412872+abhijeetsalve8997@users.noreply.github.com>
2022-04-11 19:05:58 +0530

    Arrays

---
+++ Buildings.java
@@ -0,0 +1,36 @@
+
+// Question: There are N buildings in a row with different heights H[i] (1 <= i <= N). You are viewing the buildings
+//           from the left and you can see the roof of a building i if no building to the left of the 'i'th building
+//           has a height greater than the 'i'th building.
+//           You are asked to find the number of buildings whose roofs you can see.
+//           The first line contains N denoting number of buildings.
+//           The next line contains N space seperated integers denoting heights of the buildings from left to right.
+// Input:    N = 5, arr[] = {1,2,2,4,3}
+// Output:   3
+// Input:    N = 5, arr[] = {1,2,3,4,5}
+// Output:   5
+
+import java.util.*;
+public class Buildings {
+    public static void main (String [] args) {
+        Scanner input = new Scanner (System.in);
+        int N = input.nextInt();
+        int [] arr = new int [N];
+        for (int a = 0; a < N; a++) {
+            arr[a] = input.nextInt();
+        }
+        System.out.println(visibleBuildings(arr,N));
+    }
+
+    public static int visibleBuildings (int [] arr, int N) {
+        int count = 0;
+        int max = 0;
+         for (int i = 0; i < N; i++) {
+             if (arr[i] > max) {
+                 max = arr[i];
+                 count++;
+             }
+         }
+         return count;
+    }
+}
