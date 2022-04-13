739003ac463765da6555cd0ef487c6ce111a0c23
Abhijeet Salve <101412872+abhijeetsalve8997@users.noreply.github.com>
2022-04-13 11:32:13 +0530

    1D Array

---
+++ Max_Numbers.java
@@ -0,0 +1,27 @@
+// Question: Given an array A of size N, you need to find its maximum, 2nd maximum and 3rd maximum element.
+//           Try solving it in O(N) per test case
+// Input:    size of array = 5, arr[] = {1,4,2,4,5}
+// Output:   5,4,4
+// Input:    size of array = 6, arr[] = {1,3,5,7,9,8}
+// Output:   9,8,7
+// Input:    size of array = 7, arr[] = {11,22,33,44,55,66,77}
+// Output:   77,66,55
+import java.util.*;
+public class Max_Numbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int tests = input.nextInt();
+        while (tests > 0) {
+            int size = input.nextInt();
+            int [] arr = new int [size];
+            for (int a = 0; a < arr.length; a++) {
+                arr[a] = input.nextInt();
+            }
+            Arrays.sort(arr);
+            for (int b = arr.length-1; b > arr.length-4; b--) {
+                System.out.print(arr[b]+" ");
+            }
+            tests--;
+        }
+    }
+}
