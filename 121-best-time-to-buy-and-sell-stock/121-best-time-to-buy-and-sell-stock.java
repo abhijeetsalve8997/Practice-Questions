15cf6a894499ebd6a268c353e6b23302d1b6e84e
Abhijeet Salve <101412872+abhijeetsalve8997@users.noreply.github.com>
2022-06-24 16:48:39 +0530

    Time: 3 ms (62.33%), Space: 76.2 MB (78.55%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,34 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        
+        // APPROACH 01: BRUTFORCE APPROACH (NESTED LOOPS)
+        
+        // APPROACH 02: T.C O(N) S.C O(1)
+        // int maxProfit = 0;
+        // int currProfit = 0;
+        // int minPrice = prices[0];
+        // for (int i = 1; i < prices.length; i++) {
+        //     if (prices[i] < minPrice) {
+        //         minPrice = prices[i];
+        //     }
+        //     else {
+        //         currProfit = prices[i] - minPrice;
+        //         if (maxProfit < currProfit) {
+        //             maxProfit = currProfit;
+        //         }
+        //     }
+        // }
+        // return maxProfit;
+        
+        // APPROACH 03: 
         int maxProfit = 0;
-        int currProfit = 0;
-        int minPrice = prices[0];
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < minPrice) {
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (minPrice > prices[i]) {
                 minPrice = prices[i];
             }
-            else {
-                currProfit = prices[i] - minPrice;
-                if (maxProfit < currProfit) {
-                    maxProfit = currProfit;
-                }
+            else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
             }
         }
         return maxProfit;
