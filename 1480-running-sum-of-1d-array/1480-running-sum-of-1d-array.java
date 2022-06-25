0b6cc38a11d1fff8f32315a661ca49621110bb18
Abhijeet Salve <101412872+abhijeetsalve8997@users.noreply.github.com>
2022-06-25 15:46:06 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (81.77%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -8,9 +8,3 @@ class Solution {
         return nums;
     }
 }
-
-// Time Complexity: O(n);
-
-// Space Complexity: O(n);
-
-// Explaination: In this question, firstly a new array 'cumulative[]' is declared so that all cumulative sum is stored in that array with respective its corresponding index and then it is achieved by using while loop and finally that array is returned!
